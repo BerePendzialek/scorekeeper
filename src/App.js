838047880ed887cd86475f2b4ce7d17208ed86ad
Berenice Pendzialek <berenice.pendzialek@gmail.com>
2021-02-22 15:34:36 +0100

    add plain PlayerForm component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css'
+import PlayerForm from './components/PlayerForm/PlayerForm'
 
 function App() {
   return (
     <div className="App">
       <p>Hello</p>
+      <PlayerForm />
     </div>
   )
 }
---
+++ src/components/PlayerForm/PlayerForm.js
@@ -0,0 +1,15 @@
+import './PlayerForm.css'
+import React from 'react'
+
+export default function PlayerForm() {
+  return (
+    <>
+      <section className="PlayerForm">
+        <label>
+          <h2>Add player:</h2> <br></br>
+          <input placeholder="Player name"></input>
+        </label>
+      </section>
+    </>
+  )
+}
