dc65220d9c70cecb89d7dc5b8fd3322001f4285f
Berenice Pendzialek <berenice.pendzialek@gmail.com>
2021-02-23 09:15:24 +0100

    add extra components to app,js

--- src/App.js
+++ src/App.js
@@ -35,6 +35,8 @@ function App() {
         onClick={() => console.log('Reset all')}
       ></Button>
 
+      <p>_______________</p>
+
       <h2>Scorekeeper version 2</h2>
 
       <Input labelText="Name of the game:" placeholderText="eg. Carcassonne" />
@@ -53,7 +55,32 @@ function App() {
         pages={['Play', 'History']}
       />
 
+      <p>_______________</p>
+
       <AppHeader text="Carcassonne" />
+
+      <Player
+        name="John Doe"
+        score="20"
+        onPlus={() => console.log('onPlus')}
+        onMinus={() => console.log('onMinus')}
+      />
+
+      <Player
+        name="Jane Doe"
+        score="30"
+        onPlus={() => console.log('onPlus')}
+        onMinus={() => console.log('onMinus')}
+      />
+
+      <Button
+        text="Reset scores"
+        onClick={() => console.log('Reset scores')}
+      ></Button>
+
+      <Button text="End game" onClick={() => console.log('Reset all')}></Button>
+
+      <p>_______________</p>
 
       <HistoryEntry
         nameOfGame="Carcassonne"
@@ -61,6 +88,20 @@ function App() {
           { name: 'John Doe', score: '20' },
           { name: 'Jane Doe', score: '30' },
         ]}
+      />
+
+      <HistoryEntry
+        nameOfGame="Wingspan"
+        players={[
+          { name: 'John Doe', score: '30' },
+          { name: 'Jane Doe', score: '20' },
+        ]}
+      />
+
+      <Navigation
+        onNavigate={index => console.log(index)}
+        activeIndex={0}
+        pages={['Play', 'History']}
       />
     </div>
   )
