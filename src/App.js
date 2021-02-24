3034e773e17605adf3a68ddc00a647460d0381ca
Berenice Pendzialek <berenice.pendzialek@gmail.com>
2021-02-24 16:29:41 +0100

    add navigation and create game logic

--- src/App.js
+++ src/App.js
@@ -1,89 +1,63 @@
 import { useState } from 'react'
 import styled from 'styled-components/macro'
 import Button from './components/Button/Button'
-import AppHeader from './components/Header/Header'
+import GameForm from './components/GameForm/GameForm'
+import Header from './components/Header/Header'
 import HistoryEntry from './components/HistoryEntry/HistoryEntry'
-import Input from './components/Input/Input'
 import Navigation from './components/Navigation/Navigation'
 import Player from './components/Player/Player'
-import PlayerForm from './components/PlayerForm/PlayerForm'
 
 export default function App() {
   const [players, setPlayers] = useState([])
+  const [currentPage, setCurrentPage] = useState('play')
+  const [nameOfGame, setNameOfGame] = useState('')
+
   return (
     <AppLayout>
-      <PlayerForm onAddPlayer={handleAddPlayer} />
-      {players.map(({ name, score }, index) => (
-        <Player
-          key={name}
-          name={name}
-          score={score}
-          onPlus={() => handlePlus(index)}
-          onMinus={() => handleMinus(index)}
-        />
-        // React.createElement(Player, {name, score, onPlus: () => handlePlus(index)})
-      ))}
-      <ButtonGrid>
-        <Button onClick={resetScores}>Reset scores</Button>
-        <DangerButton onClick={resetAll}>Reset all</DangerButton>
-      </ButtonGrid>
+      {currentPage === 'play' && (
+        <div>
+          <GameForm onCreateGame={createGame} />
+        </div>
+      )}
+      {currentPage === 'game' && (
+        <div>
+          <Header> {nameOfGame}</Header>
+          {players.map(({ name, score }, index) => (
+            <Player
+              key={name}
+              name={name}
+              score={score}
+              onPlus={() => handlePlus(index)}
+              onMinus={() => handleMinus(index)}
+            />
+          ))}
+          <Button onClick={resetScores}>Reset scores</Button>
+          <Button onClick={() => console.log('end game')}>End game</Button>
+        </div>
+      )}
+      {currentPage === 'history' && (
+        <div>
+          <HistoryEntry
+            nameOfGame="Carcassonne"
+            players={[
+              { name: 'John Doe', score: '20' },
+              { name: 'Jane Doe', score: '30' },
+            ]}
+          />
+        </div>
+      )}
 
-      <p>_______________</p>
-
-      <h2>Scorekeeper version 2</h2>
-
-      <Input
-        labelText="Name of the game:"
-        placeholder="eg. Carcassonne"
-        name="game"
-      />
-      <Input labelText="Player names:" placeholder="eg. John Doe, Jane Doe" />
-      <Button onClick={() => console.log('Create game')}>Create game</Button>
-
-      <Navigation onNavigate={index => console.log(index)} activeIndex={1} />
-
-      <p>_______________</p>
-
-      <AppHeader text="Carcassonne" />
-
-      <Player
-        name="John Doe"
-        score="0"
-        onPlus={() => console.log('onPlus')}
-        onMinus={() => console.log('onMinus')}
-      />
-
-      <Player
-        name="Jane Doe"
-        score="30"
-        onPlus={() => console.log('onPlus')}
-        onMinus={() => console.log('onMinus')}
-      />
-
-      <Button onClick={resetScores}>Reset scores</Button>
-      <DangerButton onClick={resetAll}>End game</DangerButton>
-
-      <p>_______________</p>
-
-      <HistoryEntry
-        nameOfGame="Carcassonne"
-        players={[
-          { name: 'John Doe', score: '20' },
-          { name: 'Jane Doe', score: '30' },
-        ]}
-      />
-
-      <HistoryEntry
-        nameOfGame="Wingspan"
-        players={[
-          { name: 'John Doe', score: '30' },
-          { name: 'Jane Doe', score: '20' },
-        ]}
-      />
-
-      <Navigation onNavigate={index => console.log(index)} activeIndex={0} />
+      {(currentPage === 'play' || currentPage === 'history') && (
+        <Navigation currentPage={currentPage} onNavigate={setCurrentPage} />
+      )}
     </AppLayout>
   )
+
+  function createGame({ nameOfGame, playerNames }) {
+    setNameOfGame(nameOfGame)
+    setPlayers(playerNames.map(name => ({ name, score: 0 })))
+    setCurrentPage('game')
+  }
 
   function handleAddPlayer(name) {
     setPlayers(oldPlayers => [...oldPlayers, { name, score: 0 }])
@@ -121,13 +95,4 @@ const AppLayout = styled.div`
   padding: 20px;
   gap: 20px;
   overflow-y: scroll;
-`
-const DangerButton = styled(Button)`
-  background-color: mistyrose;
-  border: 1px solid red;
-`
-const ButtonGrid = styled.div`
-  display: grid;
-  gap: 5px;
-  grid-template-columns: 1fr 1fr;
 `
--- src/components/Input/Input.js
+++ src/components/Input/Input.js
@@ -3,21 +3,17 @@ import styled from 'styled-components/macro'
 
 export default function Input({ labelText, placeholder, name }) {
   return (
-    <styledLabel>
+    <StyledLabel>
       {labelText}
-      <styledInput
-        name={name}
-        placeholder={placeholder}
-        type="text"
-      ></styledInput>
-    </styledLabel>
+      <StyledInput name={name} placeholder={placeholder} />
+    </StyledLabel>
   )
 }
 
-const styledLabel = styled.label`
+const StyledLabel = styled.label`
   display: grid;
   gap: 4px;
 `
-const styledInput = styled.input`
+const StyledInput = styled.input`
   border: 2px solid #bbb;
 `
